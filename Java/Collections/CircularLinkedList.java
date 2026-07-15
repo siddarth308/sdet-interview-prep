92f86bc2bb708c81fc01db8b5f696b3a648afbed
Siddarth Awasthi <siddarthawas308@gmail.com>
2026-07-16 01:45:06 +0530

    LinkedList

--- Java/Collections/CircularLinkedList.java
+++ Java/Collections/CircularLinkedList.java
@@ -1,5 +1,20 @@
 package Java.Collections;
 
+class Node3 {
+    int data;
+    Node3 next;
+
+    Node3(int new_data) {
+        this.data = new_data;
+        this.next = null;
+    }
+}
+
 public class CircularLinkedList {
+    // https://www.geeksforgeeks.org/dsa/circular-linked-list/
+
+    public static void main(String[] args) {
+
+    }
 
 }
