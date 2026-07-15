80f3d31bb3719b1a65784f2d320029afa4b3cf36
Siddarth Awasthi <siddarthawas308@gmail.com>
2026-07-16 01:58:05 +0530

    LinkedList2

--- Java/Collections/CircularLinkedList.java
+++ Java/Collections/CircularLinkedList.java
@@ -3,10 +3,12 @@ package Java.Collections;
 class Node3 {
     int data;
     Node3 next;
+    Node3 prev;
 
     Node3(int new_data) {
         this.data = new_data;
         this.next = null;
+        this.prev = null;
     }
 }
 
@@ -14,6 +16,47 @@ public class CircularLinkedList {
     // https://www.geeksforgeeks.org/dsa/circular-linked-list/
 
     public static void main(String[] args) {
+
+        // Singly Circular -
+
+        // Node3 head = new Node3(10);
+
+        // head.next = new Node3(20);
+
+        // head.next.next = new Node3(30);
+
+        // head.next.next.next = new Node3(40);
+
+        // head.next.next.next.next = head;
+
+        // Node3 temp = head;
+        // while (temp != null) {
+        // System.out.println(temp.data);
+        // temp = temp.next;
+        // }
+
+        // Doubly Circular -
+
+        Node3 head = new Node3(10);
+
+        head.next = new Node3(20);
+        head.next.prev = head;
+
+        head.next.next = new Node3(30);
+        head.next.next.prev = head.next;
+
+        head.next.next.next = new Node3(40);
+        head.next.next.next.prev = head.next.next;
+
+        Node3 tail = head.next.next.next;
+        tail.next = head;
+        head.prev = tail;
+
+        Node3 temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
 
     }
 
