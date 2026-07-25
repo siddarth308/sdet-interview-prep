c5e9178bee25dd66bb887276afd19a61ebfc8556
Siddarth Awasthi <siddarthawas308@gmail.com>
2026-07-26 01:03:21 +0530

    mergesortedArray

---
+++ DSA/Arrays/MergeSortedArray.java
@@ -0,0 +1,40 @@
+public class MergeSortedArray {
+
+    // using 2 pointers
+
+    static void mergedArray(int[] arr1, int[] arr2) {
+
+        int n = arr1.length, m = arr2.length;
+
+        int i = 0, j = 0;
+
+        int[] merged = new int[n + m];
+
+        int k = 0;
+
+        while (i < n && j < m) {
+            if (arr1[i] < arr2[j]) {
+                merged[k++] = arr1[i++];
+            } else {
+                merged[k++] = arr2[j++];
+            }
+        }
+
+        while (i < n) {
+            merged[k++] = arr1[i++];
+        }
+
+        while (j < m) {
+            merged[k++] = arr2[j++];
+        }
+
+        for (i = 0; i < n; i++) {
+            arr1[i] = merged[i];
+        }
+
+        for (j = 0; j < m; j++) {
+            arr2[j] = merged[n + j];
+        }
+    }
+
+}
