d842cef6835e687976570c56702c0a2b49797853
Siddarth Awasthi <siddarthawas308@gmail.com>
2026-07-26 23:22:20 +0530

    HashMap

--- Java/Collections/HashMapDemo.java
+++ Java/Collections/HashMapDemo.java
@@ -1,5 +1,26 @@
+// https://www.geeksforgeeks.org/java/java-util-hashmap-in-java-with-examples/
+
 package Java.Collections;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapDemo {
+
+    public static void main(String[] args) {
+
+        // Create a HashMap
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        // Add elements to the HashMap
+        hashMap.put("John", 25);
+        hashMap.put("Jane", 30);
+        hashMap.put("Jim", 35);
+
+        // Iterate through the HashMap
+        for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
 
 }
