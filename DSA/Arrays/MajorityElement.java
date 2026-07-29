846936cb97710193da25ff5a18c4ff84670b1611
Siddarth Awasthi <siddarthawas308@gmail.com>
2026-07-29 23:57:44 +0530

    MajorityElement

--- DSA/Arrays/MajorityElement.java
+++ DSA/Arrays/MajorityElement.java
@@ -95,3 +95,27 @@ static int MajorityElement(int arr[]) {
         return -1;
     }
 }
+
+// more optimal, no need of second loop -
+
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+}
