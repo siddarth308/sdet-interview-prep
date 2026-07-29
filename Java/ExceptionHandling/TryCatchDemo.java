d2b9e7026333e92e0aec3d1b913de310dfe3d2db
Siddarth Awasthi <siddarthawas308@gmail.com>
2026-07-30 00:56:24 +0530

    Day 9: Solved Majority Element and learned try, catch, finally

---
+++ Java/ExceptionHandling/TryCatchDemo.java
@@ -0,0 +1,41 @@
+// https://www.geeksforgeeks.org/java/exceptions-in-java/
+
+package Java.ExceptionHandling;
+
+public class TryCatchDemo {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Outer try block started");
+
+            // Inner try block 1
+            try {
+                int n = 10;
+                int res = n / 0;
+            } catch (ArithmeticException e) {
+                System.out.println(
+                        "Caught ArithmeticException: "
+                                + e.getClass().getName());
+            }
+
+            // Inner try block 2
+            try {
+                String s = null;
+                System.out.println(s.length());
+            } catch (NullPointerException e) {
+                System.out.println(
+                        "Caught NullPointerException: "
+                                + e.getClass().getName());
+            }
+        } catch (Exception e) {
+            System.out.println(
+                    "Caught exception in outer try-catch: "
+                            + e);
+        } finally {
+            System.out.println("Finally block executed");
+        }
+
+        System.out.println(
+                "Program continues after nested try-catch");
+    }
+}
