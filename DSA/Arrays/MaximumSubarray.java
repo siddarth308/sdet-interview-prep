b615cadd969b913b6a5d99a5668ef4e844982f57
Siddarth Awasthi <siddarthawas308@gmail.com>
2026-08-05 00:45:43 +0530

    maximumSubarray

---
+++ DSA/Arrays/MaximumSubarray.java
@@ -0,0 +1,30 @@
+public class MaximumSubarray {
+
+    static int MaxSubarray(int[] arr) {
+
+        int res = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            int currsum = 0;
+
+            for (int j = i; j < arr.length; j++) {
+                currsum = arr[j] + currsum;
+                res = Math.max(currsum, res);
+            }
+
+        }
+
+        return res;
+    }
+
+    // Using Kadane's Algorithm
+    static int MaxSubarray(int[] arr) {
+
+        int res = arr[0];
+        int maxEnding = arr[0];
+
+        for(int i=1; i< arr.length; i++){
+            maxEnding = Math.max(maxEnding + arr[i], arr[i]);
+        }
+        return res;
+}
