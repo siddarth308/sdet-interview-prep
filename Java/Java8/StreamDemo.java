95c088895180c88fed18564c2d9250438a4bea60
Siddarth Awasthi <siddarthawas308@gmail.com>
2026-08-05 00:11:09 +0530

    Stream Operators

--- Java/Java8/StreamDemo.java
+++ Java/Java8/StreamDemo.java
@@ -2,18 +2,32 @@
 
 package Java.Java8;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class StreamDemo {
-    List<String> list = Arrays.asList("apple", "banana", "grapes");
-    Stream<String> myStream = list.stream();
 
-    String[] array = { "apple", "banana", "grapes" };
-    Stream<String> Stream = Arrays.stream(array);
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("apple", "banana", "grapes");
+        Stream<String> myStream = list.stream();
 
-    Stream<Integer> IntStream = Stream.of(1, 2, 3, 4);
+        String[] array = { "apple", "banana", "grapes" };
+        Stream<String> arrayStream = Arrays.stream(array);
 
+        Stream<Integer> intStream = Stream.of(1, 2, 3, 4);
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 4322, 9, 0, 5, 44, 23, 433);
+
+        List<Integer> filteredlList = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        System.out.println(filteredlList);
+
+        List<Integer> MappedList = filteredlList.stream().map(x -> x / 2).collect(Collectors.toList());
+
+        // OR
+        list.stream().filter(x -> x % 2 == 0).map(x -> x / 2).collect(Collectors.toList());
+
+    }
 }
--- Java/Java8/StreamOperationsDemo.java
+++ Java/Java8/StreamOperationsDemo.java
@@ -4,15 +4,27 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamOperationsDemo {
 
     public static void main(String[] args) {
 
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 4322, 9, 0, 5, 44, 23, 433);
+        List<Integer> filteredList = list.stream()
+                .filter(x -> x % 2 == 0).map(x -> x / 2)
+                .distinct()
+                .sorted((a, b) -> (b - a))
+                .limit(2)
+                .skip(1)
+                .collect(Collectors.toList());
+        System.out.println(filteredList);
 
-        List<Integer> filteredlList = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
-        System.out.println(filteredlList);
-
+        Stream.iterate(0, x -> x + 1).limit(100).skip(1)
+                .filter(x -> x % 2 == 0)
+                .map(x -> x / 10)
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
