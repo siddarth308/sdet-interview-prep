16883e68f944a3058cce530bd41cdf6d4a364c8a
Siddarth Awasthi <siddarthawas308@gmail.com>
2026-08-04 23:49:29 +0530

    stream

--- Java/Java8/StreamOperationsDemo.java
+++ Java/Java8/StreamOperationsDemo.java
@@ -11,6 +11,8 @@ public class StreamOperationsDemo {
 
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 4322, 9, 0, 5, 44, 23, 433);
 
-        list.stream().filter(x -> x % 2 == l0).collect(Collectors.toList());
+        List<Integer> filteredlList = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        System.out.println(filteredlList);
+
     }
 }
