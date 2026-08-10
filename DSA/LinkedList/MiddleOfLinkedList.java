ec31f2310d03f6e7573f2d9291d30a48d9a1519e
Siddarth Awasthi <siddarthawas308@gmail.com>
2026-08-11 00:02:58 +0530

    MiddleLinkedList

---
+++ DSA/LinkedList/MiddleOfLinkedList.java
@@ -0,0 +1,16 @@
+package DSA.LinkedList;
+
+public class MiddleOfLinkedList {
+
+    public ListNode MiddleLinkedList(ListNode head) {
+
+        ListNode fastptr = head;
+        ListNode slowptr = head;
+
+        while (fastptr != null && fastptr.next != null) {
+            fastptr = fastptr.next.next;
+            slowptr = slowptr.next;
+        }
+        return slowptr;
+    }
+}
