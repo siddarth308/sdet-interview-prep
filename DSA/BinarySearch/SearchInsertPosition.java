15aaee8eff0912bb59d95fd20179a7ff85e1de84
Siddarth Awasthi <siddarthawas308@gmail.com>
2026-08-14 00:09:48 +0530

    SearchInsertPosition

--- DSA/BinarySearch/SearchInsertPosition.java
+++ DSA/BinarySearch/SearchInsertPosition.java
@@ -1,8 +1,9 @@
 package DSA.BinarySearch;
 
 public class SearchInsertPosition {
-    public int search(int[] nums, int target) {
+    public int searchInsert(int[] nums, int target) {
         int low = 0, high = nums.length - 1;
+
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
@@ -14,7 +15,6 @@ public class SearchInsertPosition {
                 high = mid - 1;
             }
         }
-        return -1;
+        return low;
     }
-
 }
