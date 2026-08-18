95a6066956cb594bb56e3f66728d090cc0aa29e8
Siddarth Awasthi <siddarthawas308@gmail.com>
2026-08-19 00:18:51 +0530

    EvaluateRPN

--- DSA/Stack/EvaluateReversePolishNotation.java
+++ DSA/Stack/EvaluateReversePolishNotation.java
@@ -4,6 +4,37 @@ public class EvaluateReversePolishNotation {
 
     public static int eval(String[] tokens) {
 
-    }
+        int i = 0;
+        int lastNum = -1;
 
+        while (i < tokens.length) {
+            if (tokens[i].equals("+") ||
+                    tokens[i].equals("-") ||
+                    tokens[i].equals("*") ||
+                    tokens[i].equals("/")) {
+                int val1 = Integer.valueOf(tokens[lastNum - 1]);
+                int val2 = Integer.valueOf(tokens[lastNum]);
+
+                int ans = 0;
+                if (tokens[i].equals("*")) {
+                    ans = val1 * val2;
+                } else if (tokens[i].equals("/")) {
+                    ans = val1 / val2;
+                } else if (tokens[i].equals("+")) {
+                    ans = val1 + val2;
+                } else if (tokens[i].equals("-")) {
+                    ans = val1 - val2;
+                }
+
+                tokens[lastNum - 1] = Integer.toString(ans);
+                lastNum--;
+            } else {
+                lastNum++;
+                tokens[lastNum] = tokens[i];
+            }
+            i++;
+
+        }
+        return Integer.valueOf(tokens[lastNum]);
+    }
 }
