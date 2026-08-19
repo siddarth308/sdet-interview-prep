a56e1cbbff9b70d217634295d578d5579de564b0
Siddarth Awasthi <siddarthawas308@gmail.com>
2026-08-19 23:50:01 +0530

    nextGreaterElement

--- DSA/Stack/DailyTemperatures.java
+++ DSA/Stack/DailyTemperatures.java
@@ -5,24 +5,26 @@ import java.util.Stack;
 
 public class DailyTemperatures {
 
-    static void dailyTemp(int[] T) {
-        int n = T.length;
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
 
         int[] daysWait = new int[n];
-
-        Arrays.fill(daysWait, -1);
 
         Stack<Integer> s = new Stack<>();
 
         for (int i = 0; i < n; i++) {
 
-            while (!s.isEmpty() && T[s.peek()] < T[i]) {
+            while (!s.isEmpty() &&
+                    temperatures[s.peek()] < temperatures[i]) {
+
                 daysWait[s.peek()] = i - s.peek();
 
                 s.pop();
             }
+
             s.push(i);
         }
-    }
 
+        return daysWait;
+    }
 }
