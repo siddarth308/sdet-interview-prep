61bae2aaf201c2b5a377e3cdae790e43e0e2ec0b
Siddarth Awasthi <siddarthawas308@gmail.com>
2026-09-03 23:21:39 +0530

    SlidingWindow

---
+++ DSA/SlidingWindow/MaximumAverageSubarrayI.java
@@ -0,0 +1,25 @@
+package DSA.SlidingWindow;
+
+public class MaximumAverageSubarrayI {
+
+    public double findMaxAverage(int[] nums, int k) {
+
+        int windowSum = 0;
+
+        for (int i = 0; i < k; i++) {
+            windowSum += nums[i];
+        }
+
+        int maxSum = windowSum;
+
+        for (int i = k; i < nums.length; i++) {
+            windowSum += nums[i];
+            windowSum = windowSum - nums[i - k] + nums[i];
+
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return (double) maxSum / k;
+
+    }
+}
