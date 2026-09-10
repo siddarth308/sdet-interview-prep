d5848d634ec68c7c58bf15b2d66340acac77fc30
Siddarth Awasthi <siddarthawas308@gmail.com>
2026-09-10 23:32:14 +0530

    NextGreaterElementI

--- DSA/SlidingWindow/LongestRepeatingCharacterReplacement.java
+++ DSA/SlidingWindow/LongestRepeatingCharacterReplacement.java
@@ -2,21 +2,35 @@ package DSA.SlidingWindow;
 
 public class LongestRepeatingCharacterReplacement {
 
-    public int lengthOfLongestSubstring(String s) {
+    public int characterReplacement(String s, int k) {
 
-        HashSet<Character> set = new HashSet<>();
+        int[] count = new int[26];
 
         int left = 0;
+        int maxFreq = 0;
         int res = 0;
 
         for (int right = 0; right < s.length(); right++) {
 
-            while (set.contains(s.charAt(right))) {
-                set.remove(s.charAt(left));
-                left++;
-            }
+            count[s.charAt(right) - 'A']++;
 
-            set.add(s.charAt(right));
+            maxFreq = Math.max(maxFreq,
+                    count[s.charAt(right) - 'A']);
+
+            int windowLength = right - left + 1;
+
+            int replacements = windowLength - maxFreq;
+
+            while (replacements > k) {
+
+                count[s.charAt(left) - 'A']--;
+
+                left++;
+
+                windowLength = right - left + 1;
+
+                replacements = windowLength - maxFreq;
+            }
 
             res = Math.max(res, right - left + 1);
         }
